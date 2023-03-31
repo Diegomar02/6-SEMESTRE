2c18190b133bbc7d8495a5ab1f620f87be50f429
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-03-30 23:02:50 -0600

    padre/hijo

--- ANGULAR/ACpadreHijo/src/app/app.module.ts
+++ ANGULAR/ACpadreHijo/src/app/app.module.ts
@@ -1,16 +1,15 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { GokuComponent } from './goku/goku.component';
+import { GohanComponent } from './gohan/gohan.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+  imports:      [ BrowserModule, FormsModule ],
+  declarations: [ AppComponent, HelloComponent, GokuComponent, GohanComponent ],
+  bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
