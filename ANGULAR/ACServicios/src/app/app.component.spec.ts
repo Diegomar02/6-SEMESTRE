eb83052dc3d4a8de4ca22a7a1e9babbdd1276c97
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-04-04 22:31:53 -0600

    servicios

--- ANGULAR/ACServicios/src/app/app.component.spec.ts
+++ ANGULAR/ACServicios/src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'ACServicios'`, () => {
+  it(`should have as title 'servicio1'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('ACServicios');
+    expect(app.title).toEqual('servicio1');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('ACServicios app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('servicio1 app is running!');
   });
 });
--- ANGULAR/ACServicios/src/app/app.component.ts
+++ ANGULAR/ACServicios/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClientesService } from './clientes.service';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'ACServicios';
+  title = 'servicio1';
+
+  constructor(public clientesService: ClientesService) { }
+
+  ngOnInit() {
+    console.log(this.clientesService);
+  }
 }
