d2707b80787b66cb0594e095dceb872d2b96b4c2
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-04-04 23:22:02 -0600

    sustituir archivos servicios

--- ANGULAR/AcServicios/src/app/app.component.ts
+++ ANGULAR/AcServicios/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ClientesService } from './clientes.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'servicio1';
+
+  constructor(public clientesService: ClientesService) { }
+
+  ngOnInit() {
+    console.log(this.clientesService);
+  }
 }
