a1113bc001afa2eecf6697e2cf302fdbee06c8d6
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-04-07 18:38:21 -0600

    Servicios / creación y modificación

---
+++ ANGULAR/TServices/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { MostrarComponent } from './mostrar/mostrar.component';
+import { BuscarComponent } from './buscar/buscar.component';
+
+import { HeroesService } from './shared/heroes.service';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    MostrarComponent,
+    BuscarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [HeroesService],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
