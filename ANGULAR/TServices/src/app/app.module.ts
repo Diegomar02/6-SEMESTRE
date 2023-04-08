e370cb4740c4b4f71ffc278f69e9d2148fa3c614
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-04-07 23:16:49 -0600

    servicios estilos

--- ANGULAR/TServices/src/app/app.module.ts
+++ ANGULAR/TServices/src/app/app.module.ts
@@ -6,6 +6,7 @@ import { AppComponent } from './app.component';
 import { MostrarComponent } from './mostrar/mostrar.component';
 import { BuscarComponent } from './buscar/buscar.component';
 
+//servicio
 import { HeroesService } from './shared/heroes.service';
 
 @NgModule({
