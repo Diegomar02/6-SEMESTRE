6df9a8488bd767f97098b7674d865adb18c19e61
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-04-09 12:58:30 -0600

    mostrar

--- ANGULAR/TServices/src/app/mostrar/mostrar.component.ts
+++ ANGULAR/TServices/src/app/mostrar/mostrar.component.ts
@@ -1,14 +1,28 @@
-import { Component } from '@angular/core';
-import { HeroesService } from '../shared/heroes.service';
+import { Component, OnInit } from '@angular/core';
+import { HeroesService,Heroe} from '../shared/heroes.service';
 
 @Component({
   selector: 'app-mostrar',
   templateUrl: './mostrar.component.html',
   styleUrls: ['./mostrar.component.css']
 })
-export class MostrarComponent {
+export class MostrarComponent implements OnInit {
 
-  constructor(){}
+  heroes:Heroe[];
+
+  constructor(public servicio: HeroesService){
+    this.heroes=this.servicio.getHeroes();
+    console.log(this.heroes);
+
+  }
+  /*
+  servicio: HeroesServicio;
+  contructor(public service:HeroesService){
+    this.servicio=service;
+    this.heroes=this.servicio.getHeroes();
+  }
+  */
+
 
   ngOnInit():void{
 
