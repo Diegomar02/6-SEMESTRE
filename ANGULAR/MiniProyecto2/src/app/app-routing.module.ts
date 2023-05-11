e09d37af50d92481e88ddf9ec96e9fe6200aa745
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-05-11 15:07:11 -0600

    modulos

--- ANGULAR/MiniProyecto2/src/app/app-routing.module.ts
+++ ANGULAR/MiniProyecto2/src/app/app-routing.module.ts
@@ -4,12 +4,10 @@ import { Airbnb1Component } from './airbnb1/airbnb1.component';
 
 const routes: Routes = [
   { path:"airbnb1",component:Airbnb1Component},
-  { path:"",redirectTo:'/airbnb1',pathMatch:'full'},
-  { path:"**",redirectTo:"airbnb1"}
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
