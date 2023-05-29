e6816a9123f02c1a6652d200a4161fae89f574a0
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-05-28 18:58:16 -0600

    QR

---
+++ ANGULAR/ProyectoFinal/QR/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { SafeUrl } from '@angular/platform-browser';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'QR';
+  url:SafeUrl=''
+  onCodeChange(url:SafeUrl){
+    console.log(url);
+    this.url=url;
+
+  }
+}
---
+++ ANGULAR/ProyectoFinal/QR/src/app/app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import {QRCodeModule} from 'angularx-qrcode';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    QRCodeModule
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
