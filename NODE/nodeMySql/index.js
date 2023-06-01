1a47e7b3f2670f90fe5866a1aae5bb7ce7230a0c
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-05-31 23:31:08 -0600

    sql

--- NODE/nodeMySql/index.js
+++ NODE/nodeMySql/index.js
@@ -1,14 +1,23 @@
-const express = require('express'); const bodyParser = require('body-parser') const connection = require("./conexion")
+const express = require('express'); 
+const bodyParser = require('body-parser') 
+const connection = require("./conexion")
+
 const app = express();
 const misrutas = require('./routes/rutas');
+
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(bodyParser.json()) app.use('/', misrutas);
-connection.connect((err, res) => { if (err) {
-console.log(err)
-console.log('Error de conexion con sql') return;
+
+app.use(bodyParser.json()) 
+app.use('/', misrutas);
+
+connection.connect((err, res) => { 
+    if (err) {
+        console.log(err)
+        console.log('Error de conexion con sql') 
+        return;
 }
 console.log('Conexion exitosa a la base de datos') });
-app.listen(3000, (err, res) => { if (err) {
+app.listen(4000, (err, res) => { if (err) {
 console.log('Error al levantar servidor')
 return; }
-console.log('Apis escuchando en el puerto 3000') })
+console.log('Apis escuchando en el puerto 4000') })
